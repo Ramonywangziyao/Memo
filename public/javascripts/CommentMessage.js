bfb403f8fc91ae19fb7a99dd800d5e5fd6a3f659
Ziyao Wang <tonywangziyao@gmail.com>
2019-04-07 18:00:20 +0800

    structural done. test and wire up pending for next step

--- public/javascripts/CommentMessage.js
+++ public/javascripts/CommentMessage.js
@@ -1,15 +1,17 @@
 var dataManager = require('./DataManager.js')
 
 module.exports = {
-  addCommentToProfile: function() {
+  addCommentToProfile: function(hashtag, comment) {
     return new Promise(function(resolve, reject) {
-
+      dataManager.insertCommentToProfileInDB(hashtag, comment).then(function(insertedInfo) {
+        console.log(insertedInfo)
+      })
     })
   },
 
   deleteComment: function() {
     return new Promise(function(resolve, reject) {
-
+      // implement later
     })
   }
 }
